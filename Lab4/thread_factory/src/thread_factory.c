a6d33f9fa3ac3b281ad3aa03b20e84ab7b83a002
NeilKingdom <neil@neilkingdom.xyz>
2023-02-16 15:08:25 -0500

    Fixed formatting

--- Lab4/thread_factory/src/thread_factory.c
+++ Lab4/thread_factory/src/thread_factory.c
@@ -24,97 +24,107 @@ static int num_threads;
 void sig_hndlr(int);
 void* tr_func(void*);
 
-void* tr_func(void *arg) {
-	while (true) {
-		/* Semaphore loop */
-		while ((sem_wait(psem) == -1) && (errno == EINTR)) ;; /* Blocked waiting for semaphore */
+void* tr_func(void *arg)
+{
+   while (true)
+   {
+      /* Semaphore loop */
+      while ((sem_wait(psem) == -1) && (errno == EINTR)) ;; /* Blocked waiting for semaphore */
 
-		/* Another thread decremented the semaphore */
-		printf("Thread %d has woken\n", gettid());
-		sleep(5); /* Sleep for 5 seconds */
-	}
+      /* Another thread decremented the semaphore */
+      printf("Thread %d has woken\n", gettid());
+      sleep(5); /* Sleep for 5 seconds */
+   }
 
-	return NULL; /* Unreachable */
+   return NULL; /* Unreachable */
 }
 
-void sig_hndlr(int signo) {
-	printf("Process received SIGUSR1\n");
-	if (num_threads > 0) {
-		for (int i = 0; i < num_threads; i++) {
-			pthread_cancel(thread_pool[i]);
-		}
-	}
+void sig_hndlr(int signo)
+{
+   printf("Process received SIGUSR1\n");
+   if (num_threads > 0)
+   {
+      for (int i = 0; i < num_threads; i++)
+      {
+         pthread_cancel(thread_pool[i]);
+      }
+   }
 
-	sem_unlink(SEM_NAME);
-	if (thread_pool)
-		free(thread_pool);
-	exit(EXIT_SUCCESS);
+   sem_unlink(SEM_NAME);
+   if (thread_pool)
+      free(thread_pool);
+   exit(EXIT_SUCCESS);
 }
 
-int main(void) {
-	/* Declare variables */
-	char usr_in[TR_LEN];
-	struct stat mode;
-	struct sched_param sched_param;
-	pthread_attr_t global_tr_attr;
-	pthread_t thread;
+int main(void)
+{
+   /* Declare variables */
+   char usr_in[TR_LEN];
+   struct stat mode;
+   struct sched_param sched_param;
+   pthread_attr_t global_tr_attr;
+   pthread_t thread;
 
-	printf("Starting process. PID: %d\n", getpid());
+   printf("Starting process. PID: %d\n", getpid());
 
-	/* Ensure we have the correct permissions */
-	if (procmgr_ability(0,
-			(PROCMGR_ADN_ROOT | PROCMGR_AOP_ALLOW | PROCMGR_AID_PRIORITY),
-			PROCMGR_AID_EOL) != EOK) {
-		fprintf(stderr, "This process does not have the proper permissions to be executed\n");
-		exit(EXIT_FAILURE);
-	}
+   /* Ensure we have the correct permissions */
+   if (procmgr_ability(0, (PROCMGR_ADN_ROOT | PROCMGR_AOP_ALLOW | PROCMGR_AID_PRIORITY), PROCMGR_AID_EOL) != EOK)
+   {
+      fprintf(stderr, "This process does not have the proper permissions to be executed\n");
+      exit(EXIT_FAILURE);
+   }
 
-	/* Initialize thread attribute struct */
-	sched_param.sched_priority = 10;
-	pthread_attr_init(&global_tr_attr);
-	pthread_attr_setschedparam(&global_tr_attr, &sched_param);
-	pthread_attr_setinheritsched(&global_tr_attr, PTHREAD_EXPLICIT_SCHED);
-	pthread_attr_setschedpolicy(&global_tr_attr, SCHED_RR);
+   /* Initialize thread attribute struct */
+   sched_param.sched_priority = 10;
+   pthread_attr_init(&global_tr_attr);
+   pthread_attr_setschedparam(&global_tr_attr, &sched_param);
+   pthread_attr_setinheritsched(&global_tr_attr, PTHREAD_EXPLICIT_SCHED);
+   pthread_attr_setschedpolicy(&global_tr_attr, SCHED_RR);
 
-	/* Setup sigaction */
-	const struct sigaction sig_act = { .sa_handler = sig_hndlr };
-	if (sigaction(SIGUSR1, &sig_act, NULL) == -1) {
-		perror("sigaction()");
-		exit(EXIT_FAILURE);
-	}
+   /* Setup sigaction */
+   const struct sigaction sig_act = { .sa_handler = sig_hndlr };
+   if (sigaction(SIGUSR1, &sig_act, NULL) == -1)
+   {
+      perror("sigaction()");
+      exit(EXIT_FAILURE);
+   }
 
-	/* Initialize semaphore */
-	mode.st_mode = (S_IRWXO | S_IRWXU);
-	assert(SEM_VAL < SEM_VALUE_MAX);
-	psem = sem_open(SEM_NAME, O_CREAT, mode.st_mode, SEM_VAL);
-	if (psem == SEM_FAILED) {
-		perror("sem_open()");
-		exit(EXIT_FAILURE);
-	}
+   /* Initialize semaphore */
+   mode.st_mode = (S_IRWXO | S_IRWXU);
+   assert(SEM_VAL < SEM_VALUE_MAX);
+   psem = sem_open(SEM_NAME, O_CREAT, mode.st_mode, SEM_VAL);
+   if (psem == SEM_FAILED)
+   {
+      perror("sem_open()");
+      exit(EXIT_FAILURE);
+   }
 
-	/* Get user input */
-	printf("Enter the number of threads: ");
-	do {
-		fflush(stdin);
-		fgets(usr_in, TR_LEN, stdin);
-		usr_in[TR_LEN - 1] = '\0';
-	} while ((num_threads = (int) strtol(usr_in, NULL, BASE)) == 0);
+   /* Get user input */
+   printf("Enter the number of threads: ");
+   do
+   {
+      fflush(stdin);
+      fgets(usr_in, TR_LEN, stdin);
+      usr_in[TR_LEN - 1] = '\0';
+   } while ((num_threads = (int) strtol(usr_in, NULL, BASE)) == 0);
 
-	printf("Number of threads: %d\n", num_threads);
+   printf("Number of threads: %d\n", num_threads);
 
-	/* Create threads */
-	thread_pool = malloc(sizeof(pthread_t) * num_threads);
-	if (thread_pool == NULL) {
-		perror("malloc()");
-		sem_unlink(SEM_NAME);
-		exit(EXIT_FAILURE);
-	}
+   /* Create threads */
+   thread_pool = malloc(sizeof(pthread_t) * num_threads);
+   if (thread_pool == NULL)
+   {
+      perror("malloc()");
+      sem_unlink(SEM_NAME);
+      exit(EXIT_FAILURE);
+   }
 
-	for (int i = 0; i < num_threads; i++) {
-		pthread_create(&thread, &global_tr_attr, tr_func, NULL);
-		thread_pool[i] = thread;
-		printf("Thread %d created\n", thread_pool[i]);
-	}
+   for (int i = 0; i < num_threads; i++)
+   {
+      pthread_create(&thread, &global_tr_attr, tr_func, NULL);
+      thread_pool[i] = thread;
+      printf("Thread %d created\n", thread_pool[i]);
+   }
 
-	while (true) ;; /* Don't let main thread exit */
+   while (true) ;; /* Don't let main thread exit */
 }
--- Lab4/thread_waker/src/thread_waker.c
+++ Lab4/thread_waker/src/thread_waker.c
@@ -12,7 +12,8 @@
 #define SEM_VAL (unsigned)0
 #define SEM_NAME "tf_sem"
 
-int main(void) {
+int main(void)
+{
 	int num_unblock;
 	char usr_in[UBLK_LEN];
 	struct stat mode;
@@ -22,13 +23,15 @@ int main(void) {
 	mode.st_mode = (S_IRWXO | S_IRWXU);
 	assert(SEM_VAL < SEM_VALUE_MAX);
 	psem = sem_open(SEM_NAME, O_CREAT, mode.st_mode, SEM_VAL);
-	if (psem == SEM_FAILED) {
+	if (psem == SEM_FAILED)
+   {
 		perror("sem_open()");
 		exit(EXIT_FAILURE);
 	}
 
 	/* Program loop */
-	while (true) {
+	while (true)
+   {
 		/* Get user input */
 		printf("How many threads would you like to unblock? (Press 0 or q to quit): ");
 		fflush(stdin);
@@ -36,17 +39,22 @@ int main(void) {
 		usr_in[UBLK_LEN - 1] = '\0';
 		num_unblock = (int) strtol(usr_in, NULL, BASE);
 
-		if (num_unblock == 0 || num_unblock == (int) 'q') {
-			if (errno != EOK) {
+		if (num_unblock == 0 || num_unblock == (int)'q')
+      {
+			if (errno != EOK)
+         {
 				perror("strtol()");
-			} else {
+			}
+         else
+         {
 				printf("Exiting...\n");
 			}
 			sem_unlink(SEM_NAME);
 			exit(EXIT_SUCCESS);
 		}
 
-		for (int i = 0; i < num_unblock; i++) {
+		for (int i = 0; i < num_unblock; i++)
+      {
 			sem_post(psem);
 		}
 		printf("Unblocked %d threads\n", num_unblock);
